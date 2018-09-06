91b8fc01fe0f1b54de2a9e461a7d747ed4f3caa6
Miao Lin <miao100tw@gmail.com>
2018-09-06 16:44:56 +0800

    add midiTest .cpp file

--- Classes/Scenes/GameScene/GameMainLayer/GameMainLayer.cpp
+++ Classes/Scenes/GameScene/GameMainLayer/GameMainLayer.cpp
@@ -7,6 +7,11 @@
 
 #include "GameMainLayer.h"
 #include "../../../ToolBox/MidiUtility.h"
+#include "../../../ToolBox/miditest2/MidiFile.h"
+#include <iostream>
+
+using namespace smf;
+using namespace std;
 
 GameMainLayer* GameMainLayer::create()
 {
@@ -23,7 +28,31 @@ bool GameMainLayer::init()
     if ( !PushAnimLayer::init())
         return false;
     
-    MidiUtility::readMidiFromFile("002.mid");
+    std::string name = "002.mid";
+    name = FileUtils::getInstance()->fullPathForFilename(name);
+    MidiFile midifile;
+    midifile.read(name);
+    midifile.doTimeAnalysis();
+    midifile.linkNotePairs();
+    
+    int tracks = midifile.getTrackCount();
+    cout << "TPQ: " << midifile.getTicksPerQuarterNote() << endl;
+    if (tracks > 1) cout << "TRACKS: " << tracks << endl;
+    for (int track=0; track<tracks; track++) {
+        if (tracks > 1) cout << "\nTrack " << track << endl;
+        cout << "Tick\t\tSeconds\t\tDur\t\tMessage" << endl;
+        for (int event=0; event<midifile[track].size(); event++) {
+            cout << dec << midifile[track][event].tick;
+            cout << '\t' << '\t' << dec << midifile[track][event].seconds;
+            cout << '\t' << '\t' ;
+            if (midifile[track][event].isNoteOn())
+                cout << midifile[track][event].getDurationInSeconds();
+            cout << '\t' << '\t' << hex;
+            for (int i=0; i<midifile[track][event].size(); i++)
+                cout << (int)midifile[track][event][i] << ' ';
+            cout << endl;
+        }
+    }
     
     
     return true;
--- Classes/ToolBox/MidiUtility.h
+++ Classes/ToolBox/MidiUtility.h
@@ -13,9 +13,15 @@
 
 USING_NS_CC;
 
+typedef unsigned char   uchar;  //1 Byte
+typedef unsigned short  ushort; //2 Byte
+typedef unsigned long   ulong;  //4 Byte
+
 class MidiUtility {
 public:
     static MeteoMidi *readMidiFromFile(const std::string fileName);
+    static ulong readLittleEndianBytes(std::string &str, ssize_t &idx, int bytesNum);
+    //static ulong readLittleEndain4Bytes(std::string &str, ssize_t &idx);
 };
 
 #endif /* MidiUtility_h */
